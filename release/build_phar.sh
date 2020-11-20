3f6a71245b4180bdd6de5bcc67f785adcaaa4069
Timo Ebel <timo.ebel@gmail.com>
2020-11-20 12:11:50 +0100

    Possible fix for PHAR file not working after it was renamed to something else.

--- release/build_phar.sh
+++ release/build_phar.sh
@@ -2,8 +2,8 @@
 
 rm -rf php-dependency-checker.phar
 
-cd release
-wget "https://github.com/MacFJA/PharBuilder/releases/download/0.2.7/phar-builder.phar"
+cd release || return
+wget "https://github.com/MacFJA/PharBuilder/releases/download/0.2.8/phar-builder.phar"
 chmod a+x phar-builder.phar
 php -d phar.readonly=0 ./phar-builder.phar package ../composer.json
 rm phar-builder.phar
